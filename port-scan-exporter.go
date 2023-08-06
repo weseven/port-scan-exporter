39bea0500f221dfd1867cdb9b182d19a8c256a43
Daniel Simionato <weseven@gmail.com>
2023-08-06 11:42:56 +0200

    Added proper logging, general refactoring.

--- port-scan-exporter.go
+++ port-scan-exporter.go
@@ -3,19 +3,25 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
+
+// TODO: move configs to a yaml file, add config to include hostNetwork pods?
 
 // Highest port number to scan.
 const maxPort = 65535
@@ -29,55 +35,66 @@ const portScanTimeout = 100 * time.Millisecond
 // Global variable to track the exporter's health status.
 var healthy bool = true
 
-type customCollector struct {
-	podsScanned *prometheus.Desc
+// Get a list of pods on the cluster.
+func getPodList() (*v1.PodList, error) {
+	clientConfig, err := rest.InClusterConfig()
+	if err != nil {
+		kubeconfig := os.Getenv("KUBECONFIG")
+		clientConfig, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			log.Println("Error building kubeconfig:", err)
+			healthy = false
+			return nil, err
+		}
+	}
+
+	clientset, err := kubernetes.NewForConfig(clientConfig)
+	if err != nil {
+		log.Println("Error creating Kubernetes client:", err)
+		healthy = false
+		return nil, err
+	}
+
+	podList, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		log.Println("Error getting pod list:", err)
+		healthy = false
+		return nil, err
+	}
+	return podList, nil
 }
 
-func newCustomCollector() *customCollector {
-	return &customCollector{
-		podsScanned: prometheus.NewDesc(
-			"pods_scanned",
+type portScanCollector struct {
+	podsPortScanned *prometheus.Desc
+}
+
+func newPortScanCollector() *portScanCollector {
+	return &portScanCollector{
+		podsPortScanned: prometheus.NewDesc(
+			"pods_port_scanned",
 			"The number of pods scanned for open ports",
 			nil, nil,
 		),
 	}
 }
 
-func (c *customCollector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- c.podsScanned
+func (c *portScanCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- c.podsPortScanned
 }
 
-func (c *customCollector) Collect(ch chan<- prometheus.Metric) {
-	clientConfig, err := rest.InClusterConfig()
-	if err != nil {
-		kubeconfig := os.Getenv("KUBECONFIG")
-		clientConfig, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
-		if err != nil {
-			fmt.Println("Error building kubeconfig:", err)
-			healthy = false
-			return
-		}
-	}
-
-	clientset, err := kubernetes.NewForConfig(clientConfig)
-	if err != nil {
-		fmt.Println("Error creating Kubernetes client:", err)
-		healthy = false
-		return
-	}
-
-	podList, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		fmt.Println("Error getting pod list:", err)
-		healthy = false
-		return
-	}
-
+func (c *portScanCollector) Collect(ch chan<- prometheus.Metric) {
 	var wg sync.WaitGroup
 	openPorts := prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "pods_open_ports",
 		Help: "Metric has value 1 if the port specified in the port label is open for the pod.",
 	}, []string{"namespace", "pod", "port"})
+
+	podList, err := getPodList()
+	if err != nil {
+		log.Println("Error getting pod list:", err)
+		healthy = false
+		return
+	}
 
 	for _, pod := range podList.Items {
 		//exclude pods using Host network
@@ -93,7 +110,7 @@ func (c *customCollector) Collect(ch chan<- prometheus.Metric) {
 	wg.Wait()
 
 	openPorts.Collect(ch)
-	ch <- prometheus.MustNewConstMetric(c.podsScanned, prometheus.CounterValue, float64(len(podList.Items)))
+	ch <- prometheus.MustNewConstMetric(c.podsPortScanned, prometheus.CounterValue, float64(len(podList.Items)))
 }
 
 func collectOpenPorts(openPorts *prometheus.GaugeVec, namespace, podName, podIP string) {
@@ -130,9 +147,10 @@ func collectOpenPorts(openPorts *prometheus.GaugeVec, namespace, podName, podIP 
 	}
 }
 
+// Results of a port scan
 type scanResult struct {
-	port   int
-	isOpen float64
+	port   int     // Port number
+	isOpen float64 // 1 if the port is open.
 }
 
 func scanPorts(targetIP string, fromPort, toPort int, results chan<- scanResult) {
@@ -142,11 +160,12 @@ func scanPorts(targetIP string, fromPort, toPort int, results chan<- scanResult)
 		if err == nil {
 			conn.Close()
 			results <- scanResult{port: port, isOpen: 1}
-			fmt.Printf("%s:%d is open\n", targetIP, port)
+			log.Printf("%s:%d is open\n", targetIP, port)
 		}
 	}
 }
 
+// TODO: non-naive implementation of health checks
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	if healthy {
 		w.WriteHeader(http.StatusOK)
@@ -158,11 +177,32 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	customCollector := newCustomCollector()
-	prometheus.MustRegister(customCollector)
+	log.Println("Starting port-scan-exporter...")
+
+	portScanCollector := newPortScanCollector()
+	prometheus.MustRegister(portScanCollector)
 
 	http.Handle("/metrics", promhttp.Handler())
 	http.HandleFunc("/health", healthHandler)
-	fmt.Println("port-scan-exporter starting on port 8080")
-	http.ListenAndServe(":8080", nil)
+
+	// Run the exporter on a separate goroutine to keep it running in the background.
+	go func() {
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatalf("Error starting the port-scan-exporter: %v", err)
+			healthy = false
+		}
+	}()
+	log.Println("port-scan-exporter is listening on :8080")
+
+	// Graceful shutdown when receiving SIGTERM signal.
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+
+	log.Println("Received termination signal. Shutting down...")
+	// Perform any cleanup or resource release tasks here.
+
+	log.Println("port-scan-exporter has terminated.")
+	os.Exit(0)
 }
